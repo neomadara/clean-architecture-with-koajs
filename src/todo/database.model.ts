eac03ed82f949ed88cee02607707bc55c12e9c35
Cristian Gutiérrez <instanciageek@gmail.com>
2021-07-18 14:11:45 -0400

    no-history: add todo schema for mongodb

---
+++ src/todo/database.model.ts
@@ -0,0 +1,17 @@
+import {model, Schema, Model} from "mongoose";
+import {TodoEntity} from "./entity";
+
+const todoSchema = new Schema <TodoEntity>(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        completed: {
+            type: Boolean,
+            default: false
+        }
+    }
+)
+
+export const Todo = model<TodoEntity, Model<TodoEntity, {}>>("Todo", todoSchema);
